ac8fe62f43a5af08b727db81d5b031f12782997c
Swann Perarnau <swann.perarnau@imag.fr>
2010-01-22 10:44:59 +0100

    added license header

--- src/krash.c
+++ src/krash.c
@@ -1,8 +1,24 @@
-/* Copyright Swann Perarnau, 2010
+/*
+ * This file is part of KRASH.
  *
- * Contact: firstname.lastname@imag.fr
+ *  KRASH is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
  *
+ *  KRASH is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
+ *  You should have received a copy of the GNU General Public License
+ *  along with KRASH.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Copyright Swann Perarnau, 2008
+ *  Contact: firstname.lastname@imag.fr
+ */
+
+ /*
  * This program is a first attempt to code the setup
  * code from KRASH in C.
  * The setup is supposed to do the following, having root priviledges:
