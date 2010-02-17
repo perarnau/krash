db8d7d9849485bd24fe38aca91da068ae1c8d61f
Swann Perarnau <swann.perarnau@imag.fr>
2010-02-17 16:59:41 +0100

    nice macros to handle error checking

--- src/cpuinjector.cpp
+++ src/cpuinjector.cpp
@@ -29,6 +29,7 @@ static char CPU_SHARES[] = "cpu.shares";
  * A special method is here to do the _big_ cleanup (like child processes and stuff)
  */
 
+
 /* helper macro for cgroup errors */
 #define cg_error(errno) {	\
 	if(errno == 0) {	\
@@ -40,6 +41,28 @@ static char CPU_SHARES[] = "cpu.shares";
 		std::cerr << "Error: " << cgroup_strerror(errno)<< " File: " << __FILE__ << ":" << __LINE__<< std::endl;	\
 	}	\
 }
+
+#define TEST_FOR_ERROR(err,label) {	\
+	if(err) {			\
+		cg_error(err);	\
+		goto label;		\
+	}				\
+}					\
+
+#define TEST_FOR_ERROR_IGNORE(err,ignore,label) {	\
+	if(err && err != ignore) {			\
+		cg_error(err);				\
+		goto label;				\
+	}						\
+}							\
+
+#define TEST_FOR_ERROR_P(p,err,label) {	\
+	if(!p) {			\
+		err = 1;		\
+		cg_error(err);		\
+		goto label;		\
+	}				\
+}					\
 
 CPUInjector *MainCPUInjector;
 
@@ -58,8 +81,7 @@ int CPUInjector::setup(ActionsList& list) {
 	ActionsList copy;
 
 	err = setup_system();
-	if(err)
-		goto error;
+	TEST_FOR_ERROR(err,error);
 
 	/* we need to copy the list for its inspection */
 	copy = list;
@@ -74,41 +96,35 @@ int CPUInjector::setup(ActionsList& list) {
 	/* iterate through cpus to setup them */
 	for(it = cpus.begin(); it != cpus.end(); it++) {
 		err = setup_cpu(*it);
-		if(err)
-			goto error;
+		TEST_FOR_ERROR(err,error);
 	}
 	return 0;
 error:
-	cg_error(err);
 	return err;
 }
 
 int CPUInjector::create_group(struct cgroup** ret, std::string name, u_int64_t shares) {
-	int err = 1;
+	int err;
 	struct cgroup* cg;
 	struct cgroup_controller *cgc;
 
 	std::string full_path = cpu_cgroup_root + name;
 	cg = cgroup_new_cgroup(full_path.c_str());
-	if(!cg)
-		goto error;
+	TEST_FOR_ERROR_P(cg,err,error);
 
 	/* to gather data on it, we must add the cpu controller and
 	 * its cpu.shares control file */
 	cgc = cgroup_add_controller(cg,CPU_CGROUP_NAME);
-	if(!cgc)
-		goto error_free;
+	TEST_FOR_ERROR_P(cgc,err,error_free);
 
 	/* 1024 is default and a good value */
 	err = cgroup_add_value_uint64(cgc,CPU_SHARES,shares);
-	if(err)
-		goto error_free;
+	TEST_FOR_ERROR(err,error_free);
 
 	/* create the group on the filesystem, populating the cg
 	 * with current values */
 	err = cgroup_create_cgroup(cg,0);
-	if(err)
-		goto error_free;
+	TEST_FOR_ERROR(err,error_free);
 
 	*ret = cg;
 	return 0;
@@ -127,13 +143,11 @@ int CPUInjector::setup_system() {
 
 	/* init cgroup library */
 	err = cgroup_init();
-	if(err)
-		goto error;
+	TEST_FOR_ERROR(err,error);
 
 	/* create the alltasks group */
 	err = create_group(&all_cg,alltasks_groupname,1024);
-	if(err)
-		goto error;
+	TEST_FOR_ERROR(err,error);
 
 	/* migrate all tasks, using task walking functions */
 	// ok libcgroup is stupid so I need to pass it a char* and I
@@ -142,8 +156,7 @@ int CPUInjector::setup_system() {
 	if(err == ECGEOF)
 		goto end_tasks; // TODO: maybe this is bad, at least one task should be moved, no ?
 
-	if(err)
-		goto error_free;
+	TEST_FOR_ERROR(err,error_free);
 
 	do {
 		/* WE MUST SKIP ERRORS HERE, NOT ALL TASKS CAN BE MOVED IF
@@ -151,19 +164,17 @@ int CPUInjector::setup_system() {
 		cgroup_attach_task_pid(all_cg,pid); // TODO: skip getpid()
 	}
 	while((err = cgroup_get_task_next(&handle,&pid)) == 0);
-	if(err != ECGEOF)
-		goto error_free;
+	TEST_FOR_ERROR_IGNORE(err,ECGEOF,error_free);
 
 end_tasks:
 	cgroup_get_task_end(&handle);
 	return 0;
 
 error_free:
-	// create group already cleans all_cg so just use this to delete the group
+	// create_group() already cleans all_cg so just use this to delete the group
 	cgroup_delete_cgroup(all_cg,0);
 	cgroup_free(&all_cg);
 error:
-	cg_error(err);
 	return err;
 }
 
@@ -183,8 +194,7 @@ int CPUInjector::setup_cpu(unsigned int cpuid) {
 	/* create the burner cpu cgroup */
 	burner_name = cgroups_basename + itos(cpuid);
 	err = create_group(&burner,burner_name,1024);
-	if(err)
-		goto error;
+	TEST_FOR_ERROR(err,error);
 
 	/* fork a burner process */
 	burner_pid = fork();
@@ -200,8 +210,7 @@ int CPUInjector::setup_cpu(unsigned int cpuid) {
 
 	/* attach pid to the burner group */
 	err = cgroup_attach_task_pid(burner,burner_pid);
-	if(err)
-		goto error_free;
+	TEST_FOR_ERROR(err,error_free);
 
 	burners_cgs[cpuid] = burner;
 	return 0;
@@ -211,7 +220,6 @@ error_free: // we do not recover from this, too hard
 	cgroup_delete_cgroup(burner,0);
 	cgroup_free(&burner);
 error:
-	cg_error(err);
 	return err;
 }
 
@@ -229,9 +237,10 @@ int CPUInjector::apply_share(unsigned int cpuid, unsigned int share) {
 
 	/* retrieve the cpu cgroup for all and get the cpu.shares value */
 	all_cgc = cgroup_get_controller(all_cg,CPU_CGROUP_NAME);
+	TEST_FOR_ERROR_P(all_cgc,err,error);
+
 	err = cgroup_get_value_uint64(all_cgc,CPU_SHARES,&all_prio);
-	if(err)
-		goto error;
+	TEST_FOR_ERROR(err,error);
 
 	/* get the burner cgroup */
 	burner = burners_cgs[cpuid];
@@ -252,23 +261,17 @@ int CPUInjector::apply_share(unsigned int cpuid, unsigned int share) {
 
 	/* get the cpu controller */
 	burner_cgc = cgroup_get_controller(burner,CPU_CGROUP_NAME);
-	if(!burner_cgc) {
-		err = 1;
-		goto error;
-	}
+	TEST_FOR_ERROR_P(burner_cgc,err,error);
 
 	/* set the new value */
 	err = cgroup_set_value_uint64(burner_cgc,CPU_SHARES,new_prio);
-	if(err)
-		goto error;
+	TEST_FOR_ERROR(err,error);
 
 	/* force lib to rewrite values */
 	err = cgroup_modify_cgroup(burner);
-	if(err)
-		goto error;
+	TEST_FOR_ERROR(err,error);
 
 	return 0;
 error:
-	cg_error(err);
 	return err;
 }
